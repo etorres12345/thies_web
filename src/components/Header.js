45436b6e6c88e88e14bfbcfdafbc07ede068571e
etorres12345 <erik.torres1215@gmail.com>
2024-03-11 13:19:16 +0100

    update mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,48 +10,49 @@ import {
 
 function Header() {
   const handleMarkerClick = () => {};
-
   const handlePhoneClick = () => {};
-
   const handleEMailClick = () => {};
 
   return (
-    <header className="row mt-4 justify-content-center align-items-center">
-      <div className="col-3">
-        <h1 className="logo">
-          <span className="regular">thies</span>
+    <header className="row mt-4 justify-content-center align-items-center pb-3 pt-3">
+      <div className="col-sm-auto col-md-auto col-lg-3 text-center mb-lg-0">
+        <h1 className="logo bg-secondary p-3 rounded-1 overflow-hidden">
+          <span className="regular text-sm">thies</span>
           <span className="bold">hansen</span>
           <span className="normal">.de</span>
         </h1>
       </div>
-      <div className="col-8 text-right">
-        <div className="header-icons">
-          <div className="row">
-            <div className="col-sm-3" onClick={handleMarkerClick}>
-              <div className="icon-wrapper">
-                <FontAwesomeIcon icon={faMapMarkerAlt} className="icon" />
-                <span className="text">Voßbarg 10</span>
-                <span className="text"> 23689 Pansdorf</span>
+      <div className="col-lg-8 col-sm-12 col-md-8 text-right m-3">
+        <div className="header-icons p-1">
+          <div className="row m-auto">
+            <div
+              className="col-sm-6 col-md-6 col-lg-3"
+              onClick={handleMarkerClick}
+            >
+              <div className="d-flex justify-content-center align-items-center">
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  className="icon"
+                  size="2x"
+                />
+                <span className="text">Voßbarg 10, 23689 Pansdorf</span>
               </div>
             </div>
-            <div className="col-sm-3">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon icon={faPhone} className="icon" />
+            <div className="col-sm-6 col-md-6 col-lg-3 mb-2">
+              <div className="d-flex justify-content-center align-items-center">
+                <FontAwesomeIcon icon={faPhone} className="icon" size="2x" />
                 <span className="text">+49 4504 606860</span>
-                {/* <FontAwesomeIcon icon={faMobile} className="icon" />
-                <span className="text">+49 172 1700648</span> */}
               </div>
             </div>
-
-            <div className="col-sm-3">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon icon={faMobile} className="icon" />
+            <div className="col-sm-6 col-md-6 col-lg-3 mb-2">
+              <div className="d-flex justify-content-center align-items-center">
+                <FontAwesomeIcon icon={faMobile} className="icon" size="2x" />
                 <span className="text">+49 172 1700648</span>
               </div>
             </div>
-            <div className="col-sm-3">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            <div className="col-sm-6 col-md-6 col-lg-3 mb-2">
+              <div className="d-flex justify-content-center align-items-center">
+                <FontAwesomeIcon icon={faEnvelope} className="icon" size="2x" />
                 <span className="text">email@thieshansen.de</span>
               </div>
             </div>
